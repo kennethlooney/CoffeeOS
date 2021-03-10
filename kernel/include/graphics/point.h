049b7964bfb4341b8ce0f3a695ad07c97028daa3
Kenneth Looney <kenneth.looney@outlook.com>
2021-03-10 08:48:01 -0800

    Added printing strings

---
+++ kernel/include/graphics/point.h
@@ -0,0 +1,11 @@
+#ifndef COFFEE_GRAPHICS_POINT_H
+#define COFFEE_GRAPHICS_POINT_H
+
+typedef struct
+{
+    /* data */
+    unsigned int X;
+    unsigned int Y;
+} Point;
+
+#endif
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,6 +1,6 @@
 #include "graphics/framebuffer.h"
 #include "graphics/simplefonts.h"
-
+#include "graphics/point.h"
 #include <stdint.h>
  
 void putChar(FRAMEBUFFER* newBuffer, PSF1_FONT* newFont, unsigned int color, char chr, unsigned int xOffset, unsigned int yOffset)
@@ -17,9 +17,31 @@ void putChar(FRAMEBUFFER* newBuffer, PSF1_FONT* newFont, unsigned int color, cha
 		fontPtr++;
 	}
 }
+Point CursorPosition;
+
+void Print(FRAMEBUFFER* newBuffer, PSF1_FONT* newFont, unsigned int color, char* str)
+{
+	 
+
+	char* chr = str;
+	while(*chr != 0) {
+		putChar(newBuffer, newFont, color, *chr, CursorPosition.X, CursorPosition.Y);
+		CursorPosition.X+=8;
+		if(CursorPosition.X + 8 > newBuffer->Width)
+		{
+			CursorPosition.X = 0;
+			CursorPosition.Y += 18;
+		}
+		chr++;
+	}
+}
 void _start(FRAMEBUFFER* newBuffer, PSF1_FONT* newFont)
 {
-    putChar(newBuffer, newFont, 0xffffffff, 'G', 10, 10);
+    CursorPosition.X = 50;
+	CursorPosition.Y = 120;
+	for(int t = 0; t < 50; t+=1){
+		Print(newBuffer,newFont, 0xffffffff, " Hello Kernel");
+	}
 	// unsigned int y = 50;
 	// unsigned int bbp = 4;
 
